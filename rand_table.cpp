fd94271a4eb3deb5c453b32f13f16a1c69070caf
yongbok1102 <yongbok1102@gmail.com>
2016-07-25 23:05:45 +0900

    rand_table.cpp
    Generates the random number table

---
+++ rand_table.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int A[6][8];
+    
+    srand((unsigned int)time(NULL));
+    
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            A[i][j]=rand()%100000+1;
+            if(trunc(log10(A[i][j]))==0)
+                cout<<"00000"<<A[i][j]<<'\t';
+            else if(trunc(log10(A[i][j]))==1)
+                cout<<"0000"<<A[i][j]<<'\t';
+            else if(trunc(log10(A[i][j]))==2)
+                cout<<"000"<<A[i][j]<<'\t';
+            else if(trunc(log10(A[i][j]))==3)
+                cout<<"00"<<A[i][j]<<'\t';    
+           else if(trunc(log10(A[i][j]))==4)
+                cout<<'0'<<A[i][j]<<'\t';
+           else if(trunc(log10(A[i][j]))==5)
+                cout<<A[i][j]<<'\t';                                                                                   
+        }
+        cout<<'\n';
+    }
+    
+    return 0;
+}
