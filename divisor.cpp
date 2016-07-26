63f0c8de6c7aba58101d514221140af6d2780bba
yongbok1102 <yongbok1102@gmail.com>
2016-07-26 18:17:27 +0900

    divisor.cpp
    print out the divisors of an integer larger than 0 and calculate the
    number of them

---
+++ divisor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    int Sum=0;
+    
+    cout<<"약수를 구하려는 자연수를 입력하시오.\n";
+    cin>>N;
+    cout<<'\n';
+    cout<<"자연수 "<<N<<"의 약수는 다음과 같습니다.\n";
+    for(int i=1;i<=N;i++)
+    {
+        if(N%i==0){
+            cout<<i<<endl;
+            Sum++;
+        }    
+    } 
+    cout<<'\n';
+    cout<<"자연수 "<<N<<"의 약수의 개수는 "<<Sum<<"입니다.\n";
+    
+    return 0;
+}
